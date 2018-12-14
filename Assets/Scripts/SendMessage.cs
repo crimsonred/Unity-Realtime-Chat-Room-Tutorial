e57b41c64da9301e1aace4b8be4a3b55ed5d9a85
Jordan Schuetz <schuetz@jordan-schuetz.local>
2018-12-14 15:28:37 -0800

    Added Buttons and Text

--- Assets/Scripts/SendMessage.cs
+++ Assets/Scripts/SendMessage.cs
@@ -3,28 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using PubNubAPI;
 using UnityEngine.UI;
+using SimpleJSON;
+
+public class MyClass
+{
+    public string username;
+    public string text;
+}
 
 public class SendMessage : MonoBehaviour {
-
+    public static PubNub pubnub;
     public string username;
     public string chat;
     public Button SubmitButton;
     public InputField UsernameInput;
     public InputField TextInput;
+    private Text text;
     // Use this for initialization
     void Start()
     {
         Button btn = SubmitButton.GetComponent<Button>();
-        btn.onClick.AddListener(SubmitText);
+        btn.onClick.AddListener(TaskOnClick);
+
+        // Use this for initialization
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.PublishKey = "pub-c-700d6386-17c7-4439-8d18-3472814914de";
+        pnConfiguration.SubscribeKey = "sub-c-523850fa-5865-11e8-9796-063929a21258";
+
+        pnConfiguration.LogVerbosity = PNLogVerbosity.BODY;
+        pnConfiguration.UUID = Random.Range(0f, 999999f).ToString();
+
+        pubnub = new PubNub(pnConfiguration);
+        //Debug.Log(pnConfiguration.UUID);
     }
 
-    void SubmitText(){
-        var usernametext = UsernameInput.text;
-        var chattext = TextInput.text;
-    }
-	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void TaskOnClick()
+    {
+        MyClass myFireObject = new MyClass();
+        myFireObject.username = UsernameInput.text;
+        myFireObject.text = TextInput.text;
+        string fireobject = JsonUtility.ToJson(myFireObject);
+        Debug.Log("click");
+        pubnub.Publish()
+            .Channel("chatchannel")
+            .Message(fireobject)
+            .Async((result, status) =>
+            {
+                if (status.Error)
+                {
+                    Debug.Log(status.Error);
+                    Debug.Log(status.ErrorData.Info);
+                }
+                else
+                {
+                    Debug.Log(string.Format("Fire Timetoken: {0}", result.Timetoken));
+                }
+            });
+    }
 }
---
+++ Assets/pubnubscript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PubNubAPI;
+using UnityEngine.UI;
+using SimpleJSON;
+
+public class pubnubscript : MonoBehaviour
+{
+    public static PubNub pubnub;
+    public string username;
+    public string chat;
+    public InputField UsernameInput;
+    public InputField TextInput;
+    private Text text;
+    public Canvas renderCanvas;
+
+    public Text hello;
+    // Use this for initialization
+    protected void Start()
+    {
+        renderCanvas = GameObject.FindObjectOfType<Canvas>();
+        // Use this for initialization
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.PublishKey = "pub-c-700d6386-17c7-4439-8d18-3472814914de";
+        pnConfiguration.SubscribeKey = "sub-c-523850fa-5865-11e8-9796-063929a21258";
+
+        pnConfiguration.LogVerbosity = PNLogVerbosity.BODY;
+        //pnConfiguration.UUID = Random.Range(0f, 999999f).ToString();
+
+        pubnub = new PubNub(pnConfiguration);
+        //Debug.Log(pnConfiguration.UUID);
+
+        pubnub.Subscribe()
+           .Channels(new List<string>() {
+                "chatchannel"
+           })
+       .WithPresence()
+       .Execute();
+
+        int ct = 1;
+        pubnub.SusbcribeCallback += (sender, e) =>
+        {
+            SusbcribeEventEventArgs message = e as SusbcribeEventEventArgs;
+            if (message.Status != null)
+            {
+            }
+            if (message.MessageResult != null)
+            {
+                //Debug.Log(message.MessageResult.Payload);
+
+                Debug.Log(message.MessageResult.Timetoken);
+                JSONObject playerJson = (JSONObject)JSON.Parse(message.MessageResult.Payload.ToString());
+                Debug.Log(playerJson["username"]);
+
+
+                string usernameText = playerJson["username"];
+                string inputText = playerJson["text"];
+
+
+                Text tempTextBox = Instantiate(hello) as Text;
+             //Parent to the panel
+                tempTextBox.transform.SetParent(renderCanvas.transform, false);
+                //Set the text box's text element font size and style:
+                // tempTextBox.fontSize = 12;
+                //Set the text box's text element to the current textToDisplay:
+                tempTextBox.text = "text";
+
+            }
+            if (message.PresenceEventResult != null)
+            {
+                Debug.Log("In Example, SusbcribeCallback in presence" + message.PresenceEventResult.Channel + message.PresenceEventResult.Occupancy + message.PresenceEventResult.Event);
+            }
+        };
+    }
+
+}
